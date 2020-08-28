85e098135a22ab9384a37e30651920f273e0061f
MohanVallivedu <vsmohan4@gmail,com>
2020-08-28 08:57:12 -0500

    Updates

---
+++ precache-manifest.d1868288ece4056926aae17b9e33b58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb05068d96c94bc7641434f96c253b2c",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "32eb22c1acf3d4eec289",
+    "url": "/covid19/static/css/main.00f4fa84.chunk.css"
+  },
+  {
+    "revision": "309a2729b2ada0e149d5",
+    "url": "/covid19/static/js/2.46733307.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19/static/js/2.46733307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32eb22c1acf3d4eec289",
+    "url": "/covid19/static/js/main.dfaeb7c1.chunk.js"
+  },
+  {
+    "revision": "b10178fb50354d9dcb37",
+    "url": "/covid19/static/js/runtime-main.197cc154.js"
+  },
+  {
+    "revision": "8cfd29513aee6280f2b5697a795fb309",
+    "url": "/covid19/static/media/Covid19.8cfd2951.jpg"
+  }
+]);
